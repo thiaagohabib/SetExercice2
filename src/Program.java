8b3b6af6e1020b22bb8af7e81f8c7e30149fcefb
Thiago Habib <thiago.habib56@gmail.com>
2020-04-18 17:57:07 -0300

    Using operation Set on exercice

---
+++ src/Program.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
+		
+		System.out.println("How many students for course A? ");
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
+		}
+		
+		System.out.println("How many students for course B? ");
+		n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+		
+		System.out.println("How many students for course C? ");
+		n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
+		}
+		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println("Total sutdents: " + total.size());
+		
+		
+		
+		sc.close();
+	}
+
+}
